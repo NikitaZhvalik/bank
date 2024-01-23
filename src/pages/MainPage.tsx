18bbb4aec2bd193f2fe44dd9f0ae148a430ec22d
Никита Жвалик <nikitazvalik@MacBook-Pro-Nikita.local>
2024-01-24 00:35:36 +1000

    first commit

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,7 @@
+const MainPage = () => {
+    return (
+        <div>123</div>
+    );
+}
+ 
+export default MainPage;
---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainPage from "../pages/MainPage";
+
+const AppRoutes = () => {
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<MainPage />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
+}
+ 
+export default AppRoutes;
